31f5930c3d746344a36c9d708038bf5bdcf5de61
aravindkumar595 <148612710+aravindkumar595@users.noreply.github.com>
2025-12-23 16:16:03 +0530

    Initial project upload

---
+++ mysite/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+from employee.models import Employee
+
+def home(request):
+  employees=Employee.objects.all()
+  context={
+    'employees':employees,
+  }
+  return render(request,"first.html",context)
+
+
+
